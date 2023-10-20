c3681ba96e1591c02f9f3a2082c4ae33fd5aed4a
cecilia reynoso <cecireynoso@gmail.com>
2023-10-19 23:15:19 -0300

    event listener challenge

--- app.js
+++ app.js
@@ -2,23 +2,10 @@ const button = document.querySelector('button');
 const color = document.getElementById('color');
 
 const generateColor = () => {
-  let digits = '0123456789ABCDEF';
-  let hexColor = '#';
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    color.innerHTML = "#" + randomColor;
+}
 
-  for (let i = 0; i < 6; i++) {
-    let randomIndex = Math.floor(Math.random() * 16);
-    hexColor += digits[randomIndex];
-  }
-
-  return hexColor;
-
-
-};
-
-button.addEventListener('click', () => {
-  let newColor = generateColor();
-  color.textContent = newColor;
-  document.body.style.backgroundColor = newColor;
-});
-
-
+button.addEventListener('click', generateColor);
+generateColor();
